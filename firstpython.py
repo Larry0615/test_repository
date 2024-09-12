e90344e298f2cb236293f3700df64ecf83fd1f11
Larry0615 <oyeore11@gmail.com>
2024-09-12 18:13:11 +0100

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,3 @@
 # Typing a test code in python environment
 print ("New Python file")
+# God help me please in becoming one of the greatest Data Scientist and successful in terms of wealth to commensurate with it, AMEN. 
